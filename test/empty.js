8e8d165f3edbeedc1bf58759d1882be0566ed75e
Fabien Tesselaar <fabientesselaar@gmail.com>
2015-12-23 19:44:07 +0100

    Added undefined check

--- test/empty.js
+++ test/empty.js
@@ -16,6 +16,14 @@ describe('Empty', function () {
       });
   });
 
+  describe('undefined', function () {
+      var sentences = tokenizer.sentences();
+
+      it('should not get a sentence', function () {
+          assert.equal(sentences.length, 0);
+      });
+  });
+
   describe('symbols only', function () {
       var entry = "^&%(*&";
       var sentences = tokenizer.sentences(entry);
