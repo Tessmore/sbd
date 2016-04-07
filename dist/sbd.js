d063fbd3c852b59be5ac4c27582ac43e62b8a2dc
Fabien Tesselaar <fabientesselaar@gmail.com>
2016-04-07 19:08:43 +0200

    Added npm build script + updated dist

--- dist/sbd.js
+++ dist/sbd.js
@@ -1,4 +1,4 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.tokenizer=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.tokenizer = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
 var abbreviations = [
     "ie",
@@ -201,16 +201,25 @@ var newline_placeholder = " @~@ ";
 var newline_placeholder_t = newline_placeholder.trim();
 
 // Split the entry into sentences.
-exports.sentences = function(text, newline_boundary) {
-    if (text.length === 0)
+exports.sentences = function(text, options) {
+    if (!text || typeof text === "undefined" || text.length === 0)
         return [];
 
-    text = sanitizeHtml(text, { "allowedTags" : [''] });
-
-    /** Preprocessing */
-    if (typeof newline_boundary === 'undefined') {
+    /** Options processing */
+    var newline_boundary;
+    var do_sanitize = true;
+    if (typeof options === 'undefined') {
         newline_boundary = false;
     }
+    else if (typeof options === 'object') {
+      newline_boundary = options.newline_boundary || false;
+      do_sanitize = typeof options.sanitize === 'undefined' ? true : options.sanitize;
+    }
+    else {
+        newline_boundary = options;
+    }
+
+    text = do_sanitize ? sanitizeHtml(text, { "allowedTags" : [''] }) : text;
 
     if (newline_boundary) {
         text = text.replace(/\n+|[-#=_+*]{4,}/g, newline_placeholder);
@@ -233,7 +242,7 @@ exports.sentences = function(text, newline_boundary) {
         // Add the word to current sentence
         current.push(words[i]);
 
-        // Sub-sentences (Bijzin?), reset counter
+        // Sub-sentences, reset counter
         if (~words[i].indexOf(',')) {
             wordCount = 0;
         }
@@ -252,6 +261,12 @@ exports.sentences = function(text, newline_boundary) {
             current   = [];
 
             continue;
+        }
+
+
+        if (String.endsWithChar(words[i], "\"") || String.endsWithChar(words[i], "”")) {
+            // endQuote = words[i].slice(-1);
+            words[i] = words[i].slice(0, -1);
         }
 
         // A dot might indicate the end sentences
