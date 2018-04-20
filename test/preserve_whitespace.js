4afc35a8ceac01c1da1171ce1d28238df8f403fe
Chad Kirby <chad.kirby@gmail.com>
2018-04-20 11:13:08 -0700

    return correct whitespace-preserving results when a short list sentence is joined to its neighbor
    I discovered that the `preserve_whitespace` option breaks when the tokenizer encounters a single-word 'enumeration list' sentence. Basically, it needs to fix up the list sentences before interleaving the tokens and whitespace.

--- test/preserve_whitespace.js
+++ test/preserve_whitespace.js
@@ -51,4 +51,16 @@ describe('Preserve whitespace', function () {
             assert.equal(sentences[0], "This is a sentence with funny whitespace.");
         });
     });
+
+    describe('It should properly join single-word list sentences', function () {
+        var entry = "iv. determining that the advertisement in the lift study is a candidate ad for the user, computing whether to include the user in a test group or a control group for the lift study ([0032]), v. based on the computation indicating that the user is in the control group, holding out the advertisement from completing the ad selection process for the user ([0032]), and vi. based on the computation indicating that the user is in the test group, allowing the advertisement to continue through the ad selection process such that the user receives either the advertisement in the lift study or another advertisement ([0032]); and ";
+        var sentences = tokenizer.sentences(entry, options);
+
+       it("should get the correct sentences", function () {
+            assert.deepEqual(sentences, [
+              "iv. determining that the advertisement in the lift study is a candidate ad for the user, computing whether to include the user in a test group or a control group for the lift study ([0032]), v. based on the computation indicating that the user is in the control group, holding out the advertisement from completing the ad selection process for the user ([0032]), and vi. ",
+              "based on the computation indicating that the user is in the test group, allowing the advertisement to continue through the ad selection process such that the user receives either the advertisement in the lift study or another advertisement ([0032]); and "
+            ]);
+        });
+    });
 });
